8031c220191fbc8bae1b37d22d21ecb9c3e36770
matthew allen <codedtechless@gmail.com>
2022-11-21 23:40:32 +0000

    waow

--- DesecratedDungeons/src/resource/serial/byte_file.cpp
+++ DesecratedDungeons/src/resource/serial/byte_file.cpp
@@ -6,28 +6,5 @@
 namespace Desdun
 {
 	
-	ByteOutputStream::ByteOutputStream(const std::string& name)
-		: m_Name(name)
-	{
-		m_Stream.open(name, std::ios_base::binary | std::ios_base::out);
-	}
-
-	ByteInputStream::ByteInputStream(const std::string& name)
-		: m_Name(name)
-	{
-		m_Stream.open(name, std::ios_base::binary | std::ios_base::in);
-	}
-
-	ByteOutputStream::~ByteOutputStream()
-	{
-		if (m_Stream.is_open() == true)
-			m_Stream.close();
-	}
-
-	ByteInputStream::~ByteInputStream()
-	{
-		if (m_Stream.is_open() == true)
-			m_Stream.close();
-	}
 
 }
--- DesecratedDungeons/src/resource/serial/byte_file.h
+++ DesecratedDungeons/src/resource/serial/byte_file.h
@@ -9,83 +9,26 @@
 namespace Desdun
 {
 
-	namespace ByteStream
-	{
-		enum class Type
-		{
-			Null,
-			Int,
-			UnsignedInt,
-			Double,
-			Bool,
-			String
-		};
-	};
-
-	class ByteOutputStream
+	class ByteStream
 	{
 	public:
-		ByteOutputStream(const std::string& name);
-		~ByteOutputStream();
 
-		template<typename T>
-		void write(ByteStream::Type type, T* buffer = nullptr)
+		void operator>>(std::ofstream& stream)
 		{
-			char typeFinal = static_cast<char>(type);
+			
+		}
 
-			m_Stream.write((char*)&typeFinal, sizeof(char));
-
-			if (buffer)
-			{
-				m_Stream.write((char*)buffer, sizeof(T));
-			}
-		};
-
-		template<typename T>
-		void write(T* buffer, size_t count)
+		void operator<<(std::ifstream& stream)
 		{
-			m_Stream.write((char*)buffer, sizeof(T) * count);
-		};
 
-		void operator<<(const int64_t& source) 
-		{
-			write(ByteStream::Type::Int, &source);
-		};
-
-		void operator<<(const uint64_t& source)
-		{
-			write(ByteStream::Type::UnsignedInt, &source);
-		};
-
-		void operator<<(const std::string& source)
-		{
-			size_t count = source.size();
-
-			write(ByteStream::Type::String, &count);
-			write(source.c_str(), count);
 		}
 
 	private:
 
-		std::string m_Name;
-		std::ofstream m_Stream;
+		char* m_Buffer = nullptr;
 
-	};
-
-	class ByteInputStream
-	{
-	public:
-		ByteInputStream(const std::string& name)
-			: m_Name(name) {};
-
-		~ByteInputStream();
-
-
-
-	private:
-
-		std::string m_Name;
-		std::ifstream m_Stream;
+		uint m_Size = 0;
+		uint m_Reserved = 0;
 
 	};
 
