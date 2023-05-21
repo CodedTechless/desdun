1be7a4709e1e216c1b59cf0b81546f3bca9af360
matthew allen <codedtechless@gmail.com>
2023-05-21 13:20:37 +0100

    more shm stuff

---
+++ projects/DesdunEngine/src/runtime/spatial_hash_map.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <core_lib.hpp>
+
+#include <object/instance.h>
+
+
+namespace Desdun
+{
+
+	class SpatialHashMap
+	{
+	public:
+
+
+
+
+	private:
+
+		Map<Vector2, List<Instance*>> container = {};
+
+	};
+
+
+}
