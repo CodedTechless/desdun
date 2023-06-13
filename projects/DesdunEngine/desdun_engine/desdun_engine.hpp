97cb1a4db88b95632d15d71b6333f4835eebd221
matthew allen <codedtechless@gmail.com>
2023-06-13 22:28:53 +0100

    fixed linking issues

--- projects/DesdunEngine/desdun_engine/desdun_engine.hpp
+++ projects/DesdunEngine/desdun_engine/desdun_engine.hpp
@@ -2,42 +2,42 @@
 
 // App
 
-#include "include/desdun_core.hpp"
+#include <desdun_engine/include/desdun_core.hpp>
 
 // >> Debugging
 
-#include "src/app/debug/debug.h"
+#include <desdun_engine/src/app/debug/debug.h>
 
-#include "src/app/imgui/panel/scene_explorer.h"
+#include <desdun_engine/src/app/imgui/panel/scene_explorer.h>
 
 // >> Resources
 
-#include "src/app/resource/resource.hpp"
+#include <desdun_engine/src/app/resource/resource.hpp>
 
-#include "src/app/resource/descendants/audio.h"
-#include "src/app/resource/descendants/image.h"
-#include "src/app/resource/descendants/model.h"
-#include "src/app/resource/descendants/shader.h"
+#include <desdun_engine/src/app/resource/descendants/audio.h>
+#include <desdun_engine/src/app/resource/descendants/image.h>
+#include <desdun_engine/src/app/resource/descendants/model.h>
+#include <desdun_engine/src/app/resource/descendants/shader.h>
 
 // >> Scene and Objects
 
-#include "src/app/scene/scene.h"
+#include <desdun_engine/src/app/scene/scene.h>
 
 // >> Input
 
-#include "src/app/app.h"
-#include "src/app/window/input.h"
-#include "src/app/window/window.h"
+#include <desdun_engine/src/app/app.h>
+#include <desdun_engine/src/app/window/input.h>
+#include <desdun_engine/src/app/window/window.h>
 
 // Graphics and Rendering
 
 // >> Renderer	
 
-#include "src/graphics/render/renderer.h"
+#include <desdun_engine/src/graphics/render/renderer.h>
 
 // Instance
 
 // >> Index
 
-#include "src/instance/index.hpp"
+#include <desdun_engine/src/instance/index.hpp>
 
